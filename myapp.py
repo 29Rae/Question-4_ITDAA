237dccf165810d0e427a80d9e56b3eb4b6c2b2db
29Rae <110805562+29Rae@users.noreply.github.com>
2024-06-20 08:54:22 +0200

    added packages.txt

--- myapp.py
+++ myapp.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
+# import seaborn as sns
+# import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
